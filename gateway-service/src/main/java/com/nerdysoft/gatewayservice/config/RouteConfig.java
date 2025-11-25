80bb359403835c69e190862572b91b321651463e
OleksandrHelevanNS <oleksandr.helevan@nerdysoft.com>
2025-11-25 17:41:00 +0200

    Add gateway-service

---
+++ gateway-service/src/main/java/com/nerdysoft/gatewayservice/config/RouteConfig.java
@@ -0,0 +1,19 @@
+package com.nerdysoft.gatewayservice.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RouteConfig {
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes()
+                .route("menu-service", predicate ->
+                        predicate.path("/pizzas/**")
+                                .uri("lb://menu-service")
+                )
+                .build();
+    }
+}
